bcb734b1c3eb2d89dd17cc7bedcc7e47c1c06b66
João <joao.codelearning@gmail.com>
2026-02-01 23:37:58 -0300

    feat: implement flatlist on home rendering patientCard and consume patientContext as data

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,65 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, FlatList } from "react-native";
 import { Button } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { PatientCard } from "../../components/PatientCard";
+
+// contexto dos pacientes salvos
+import { useContext } from "react";
+import { PatientContext } from "../../contexts/patientContext";
 
 export default function Home() {
   const navigation = useNavigation();
+
+  // consumo do contexto
+  const { patients, loading } = useContext(PatientContext);
   return (
-    <View style={styles.container}>
-      <Button onPress={()=> navigation.navigate('GlasgowForm')}>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Lista de Pacientes ({patients.length})</Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ gap: 8 }}
+        data={patients}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <PatientCard
+            patientName={item.patientName}
+            patientAge={item.patientAge}
+            createdAt={item.createdAt}
+            result={item.resultado}
+          />
+        )}
+      />
+
+      <Button
+        style={styles.buttonView}
+        textColor="#000"
+        labelStyle={{ fontWeight: "bold", fontSize: 16 }}
+        onPress={() => navigation.navigate("GlasgowForm")}
+      >
         <Text>Adicionar</Text>
       </Button>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#f3f4f5",
+    padding: 10,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    padding: 24,
+    alignSelf: "flex-start",
+  },
+
+  buttonView: {
+    padding: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#000",
   },
 });
